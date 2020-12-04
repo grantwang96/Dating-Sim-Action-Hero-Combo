fe507d5d75145c8a6c3305008f0dac1877ba60c6
grantwang96 <gw805@nyu.edu>
2020-12-03 20:44:39 -0800

    Added look at current target, quick scan, and updates to Date and Test Enemy AI States

---
+++ Dating Sim Action Hero Combo/Assets/Scripts/AI States/AIState_LookAtCurrentTarget.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIState_LookAtCurrentTarget : AIState
+{
+    [SerializeField] private NPCTargetManager _npcTargetManager;
+    [SerializeField] private NPCNavigator _navigator;
+    [SerializeField] private AIState _onCantFindTarget;
+
+    protected override void OnEnter() {
+        base.OnEnter();
+        if(_npcTargetManager.CurrentTarget == null) {
+            Debug.LogError($"[{_unit.name}/{name}]: Current target was null!");
+            SetReadyToTransition(_onCantFindTarget);
+            return;
+        }
+        _navigator.LookTarget = _npcTargetManager.CurrentTarget.MoveController.Body;
+    }
+}
---
+++ Dating Sim Action Hero Combo/Assets/Scripts/AI States/AIState_QuckScanForTarget.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIState_QuckScanForTarget : AIState
+{
+    [SerializeField] private AIState _onTargetFound;
+    [SerializeField] private AIState _onTargetLost;
+
+    private DetectableTags _expectedDetectableTags;
+
+    protected override void OnEnter() {
+        base.OnEnter();
+        if(_unit.TargetManager.CurrentTarget == null) {
+            SetReadyToTransition(_onTargetLost);
+            return;
+        }
+        _expectedDetectableTags = _unit.TargetManager.CurrentTarget.DetectableTags;
+        bool foundTarget = ScanForTarget();
+        if (foundTarget) {
+            SetReadyToTransition(_onTargetFound);
+        } else {
+            SetReadyToTransition(_onTargetLost);
+        }
+    }
+
+    private bool ScanForTarget() {
+        Unit currentTarget = _unit.TargetManager.CurrentTarget;
+        if (currentTarget == null) {
+            SetReadyToTransition(_onTargetLost);
+            return false;
+        }
+        bool foundTarget = true;
+        foundTarget &= (currentTarget.DetectableTags & _expectedDetectableTags) != 0;
+        foundTarget &= _unit.TargetManager.CanSeeTarget(currentTarget);
+        return foundTarget;
+    }
+}
